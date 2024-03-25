5324941176e16b0825d1d39a51261bb261f72cae
myselfshravan <myselfshravan@gmail.com>
2024-03-25 13:32:06 +0530

    added upload feature

--- simpleUI.py
+++ simpleUI.py
@@ -1,7 +1,10 @@
+import os
 import streamlit as st
 from chromadb.utils.embedding_functions import OpenCLIPEmbeddingFunction
 from chromadb.utils.data_loaders import ImageLoader
 import chromadb
+import numpy as np
+from PIL import Image
 
 
 def setup_chroma_db():
@@ -16,39 +19,35 @@ def setup_chroma_db():
     return multimodal_db
 
 
-def display_search_results(results, col1, col2, col3):
+def display_search_results(results, n_results):
     if results:
-        st.write(f"Found {len(results['ids'][0])} results!")
-        with col1:
-            st.write("Result 1")
-            st.image(results['data'][0][0], caption=f"Distance: {results['distances'][0][0]}")
-            st.write(f"URI: {results['uris'][0][0]}")
-            st.write(results['metadatas'][0][0])
-        with col2:
-            st.write("Result 2")
-            st.image(results['data'][0][1], caption=f"Distance: {results['distances'][0][1]}")
-            st.write(f"URI: {results['uris'][0][1]}")
-            st.write(results['metadatas'][0][1])
-        with col3:
-            st.write("Result 3")
-            st.image(results['data'][0][2], caption=f"Distance: {results['distances'][0][2]}")
-            st.write(f"URI: {results['uris'][0][2]}")
-            st.write(results['metadatas'][0][2])
+        for idx, result in enumerate(results['data'][0]):
+            col_idx = idx % 3
+            if col_idx == 0:
+                cols = st.columns(3)
+
+            with cols[col_idx]:
+                st.write(f"Result {idx + 1}")
+                st.image(result, caption=f"Distance: {results['distances'][0][idx]}")
+                st.write(f"URI: {results['uris'][0][idx]}")
+                st.caption(results['metadatas'][0][idx])
+
+            if col_idx == 2 or idx == n_results - 1:
+                st.write("---")
 
 
-def handle_text_search(query_text, multimodal_db):
+def handle_text_search(query_text, multimodal_db, n_results=3):
     if query_text:
         st.write(f'Searching for: {query_text}')
         results = multimodal_db.query(
             query_texts=[query_text],
-            n_results=3,
+            n_results=n_results,
             include=['documents', 'distances', 'metadatas', 'data', 'uris'],
         )
-        col1, col2, col3 = st.columns(3)
-        display_search_results(results, col1, col2, col3)
+        display_search_results(results, n_results)
 
 
-def handle_uri_search(query_uri, multimodal_db):
+def handle_uri_search(query_uri, multimodal_db, n_results=3):
     if query_uri:
         st.write(f'Searching for: {query_uri}')
         st.image(query_uri, caption='Query Image', width=200)
@@ -57,12 +56,28 @@ def handle_uri_search(query_uri, multimodal_db):
         st.write(f'Code: {code}')
         results = multimodal_db.query(
             query_uris=[query_uri],
-            n_results=3,
+            n_results=n_results,
             include=['documents', 'distances', 'metadatas', 'data', 'uris'],
             where={'filename': {'$ne': code}}
         )
-        col1, col2, col3 = st.columns(3)
-        display_search_results(results, col1, col2, col3)
+        display_search_results(results, n_results)
+
+
+def handle_image_search(query_image, multimodal_db, n_results=3):
+    if query_image:
+        st.write(f'Searching for: {query_image}')
+        results = multimodal_db.query(
+            query_images=[query_image],
+            n_results=n_results,
+            include=['documents', 'distances', 'metadatas', 'data', 'uris'],
+        )
+        display_search_results(results, n_results)
+
+
+def save_uploadedfile(uploadedfile, file_name):
+    with open(os.path.join("queryImg", file_name), "wb") as f:
+        f.write(uploadedfile.getbuffer())
+    return st.success("Image saved!")
 
 
 def main():
@@ -70,14 +85,25 @@ def main():
     st.write('This is a simple image search engine using OpenCLIP embeddings and ChromaDB.')
 
     multimodal_db = setup_chroma_db()
-    option = st.radio("Select an option", ('Search by text', 'Search by URI'), horizontal=True, index=1)
+    option = st.radio("Select an option", ('Search by text', 'Search by URI', 'Search by Image'), horizontal=True,
+                      index=1)
+
+    n_results = st.number_input("Number of results to display", min_value=1, value=3, step=1)
+
     if option == 'Search by text':
         query_text = st.text_input('Enter your search query:')
-        handle_text_search(query_text, multimodal_db)
+        handle_text_search(query_text, multimodal_db, n_results=n_results)
 
     if option == 'Search by URI':
         query_uri = st.text_input('Enter the URI of the image you want to search for:')
-        handle_uri_search(query_uri, multimodal_db)
+        handle_uri_search(query_uri, multimodal_db, n_results=n_results)
+
+    if option == 'Search by Image':
+        query_image = st.file_uploader('Upload an image to search for:', type=['jpg', 'jpeg', 'png'])
+        if query_image:
+            save_uploadedfile(query_image, file_name="query_img.jpg")
+            query_image_uri = "queryImg/query_img.jpg"
+            handle_uri_search(query_image_uri, multimodal_db, n_results=n_results)
 
 
 if __name__ == "__main__":
